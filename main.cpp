291bedbbfe49f7eb30335c75aa1e07465a90eed8
Sermi09 <72806396+Sermi09@users.noreply.github.com>
2021-03-16 12:34:49 +0100

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <C:/Users\kijpi\Desktop\color-console\include\color.hpp>
+
+int main() {
+
+    std::cout << dye::aqua("Hello, World! ~ ") << dye::red("Brian Kernighan")<<std::endl;
+    std::cout << dye::green("The only impossible journey is the one you never begin. ~ ") << dye::yellow("Tony Robbins")<<std::endl;
+    std::cout << dye::purple("The purpose of our lives is to be happy. ~ ") << dye::blue("Dalai Lama")<<std::endl;
+
+    return 0;
+}
